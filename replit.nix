0eabb8d8869eda8206bdff11d4b6e22e569e062c
unlovdman <achmadr203@gmail.com>
2025-03-10 13:38:51 +0700

    p

--- replit.nix
+++ replit.nix
@@ -3,19 +3,7 @@
         pkgs.nodejs-16_x
         pkgs.chromium
         pkgs.ffmpeg
-        pkgs.wget
-        pkgs.nodePackages.typescript
-        pkgs.nodePackages.pm2
         pkgs.python3
         pkgs.git
-        pkgs.which
-        pkgs.pkg-config
-        pkgs.cairo
-        pkgs.pango
-        pkgs.libpng
-        pkgs.libjpeg
-        pkgs.giflib
-        pkgs.librsvg
-        pkgs.webp
     ];
 } 
